fe0d6c4edc9f8f7101adb1f4192499e0aa0ee15f
Ray Greenwell <ray@threerings.net>
2014-05-28 01:21:44 +0000

    Uh... that's embarassing. I thought that had compiled but it hadn't yet.
    So now you get to watch me flail.
    
    This is more better, but still not quite right...

--- src/cpp/com_threerings_froth_SteamController.cpp
+++ src/cpp/com_threerings_froth_SteamController.cpp
@@ -9,7 +9,7 @@ JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_init (
     JNIEnv* env, jclass clazz, jstring file)
 {
     const char* str = env->GetStringUTFChars(file, NULL);
-    bool retval = ISteamController()->Init();
+    bool retval = SteamController()->Init();
     env->ReleaseStringUTFChars(file, str);
     return retval;
 }
@@ -17,14 +17,14 @@ JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_init (
 JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_shutdown (
     JNIEnv* env, jclass clazz)
 {
-    return ISteamController()->Shutdown();
+    return SteamController()->Shutdown();
 }
 
 JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_getControllerState (
     JNIEnv* env, jclass clazz, jint controller, jobject state)
 {
     // TODO: NULL should be the state. this will boo-boo-boooch
-    bool retval = ISteamController()->GetControllerState((uint32)controller, NULL);
+    bool retval = SteamController()->GetControllerState((uint32)controller, NULL);
     return retval;
 }
 
@@ -32,6 +32,6 @@ JNIEXPORT void JNICALL Java_com_threerings_froth_SteamController_setOverrideMode
     JNIEnv* env, jclass clazz, jstring mode);
 {
     const char* modeStr = env->GetStringUTFChars(mode, NULL);
-    ISteamController()->SetOverrideMode(modeStr);
+    SteamController()->SetOverrideMode(modeStr);
     env->ReleaseStringUTFChars(mode, modeStr);
 }
