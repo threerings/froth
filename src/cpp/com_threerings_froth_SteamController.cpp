b228df19a3c80d70f98f2fa8d426e68c4c067681
Ray Greenwell <ray@threerings.net>
2014-05-28 22:10:55 +0000

    Allocate the state struct on the heap each call. That probably fixes any threading issues...
    I think I'm going to take a moment to read a primer on JNI...
    (now that I have everything working!)

--- src/cpp/com_threerings_froth_SteamController.cpp
+++ src/cpp/com_threerings_froth_SteamController.cpp
@@ -5,9 +5,6 @@
 
 #include "com_threerings_froth_SteamController.h"
 
-/** A static, reusabled controller state object. */
-static SteamControllerState_t _controllerState;
-
 JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_init (
     JNIEnv* env, jclass clazz, jstring file)
 {
@@ -26,18 +23,19 @@ JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_shutdown (
 JNIEXPORT jboolean JNICALL Java_com_threerings_froth_SteamController_getControllerState (
     JNIEnv* env, jclass clazz, jint controller, jobject state)
 {
-    bool retval = SteamController()->GetControllerState((uint32)controller, &_controllerState);
+    SteamControllerState_t controllerState;
+    bool retval = SteamController()->GetControllerState((uint32)controller, &controllerState);
     if (retval) {
         // TODO: make this all pre-cached for the fasterness?
         jclass stateClazz = env->FindClass("com/threerings/froth/SteamController$State");
         jmethodID stateSet = env->GetMethodID(stateClazz, "setValues", "(IJSSSS)V");
         env->CallVoidMethod(state, stateSet,
-                _controllerState.unPacketNum,
-                _controllerState.ulButtons,
-                _controllerState.sLeftPadX,
-                _controllerState.sLeftPadY,
-                _controllerState.sRightPadX,
-                _controllerState.sRightPadY);
+                controllerState.unPacketNum,
+                controllerState.ulButtons,
+                controllerState.sLeftPadX,
+                controllerState.sLeftPadY,
+                controllerState.sRightPadX,
+                controllerState.sRightPadY);
     }
     return (jboolean)retval;
 }
